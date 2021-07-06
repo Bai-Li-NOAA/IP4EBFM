ccad32536751d26bb23690ae88ed017e657fae9d
Bai Li <bai.li@noaa.gov>
2021-07-06 12:17:52 -0400

    add R function bibtex2rmd

---
+++ R/bibtex2rmd.R
@@ -0,0 +1,82 @@
+#' Read bibtex to an Rmd file.
+#'
+#' @param bibfile .bib file path
+#' @param outfold output folder for .Rmd
+#' @param outfile character string naming a file, should end in \dQuote{.Rmd}.
+#' @param abstract print abstract or not
+#' @param overwrite overwite the .Rmd file or not
+#'
+#' @export
+
+bibtex2rmd <- function(bibfile,
+                       outfold,
+                       outfile,
+                       abstract = FALSE,
+                       overwrite = FALSE) {
+
+
+  require(RefManageR)
+  require(dplyr)
+  require(stringr)
+  require(anytime)
+
+  # Import the bibtex file and convert to data.frame
+  mypubs <- ReadBib(bibfile, check = "warn", .Encoding = "UTF-8") %>%
+    as.data.frame()
+
+  filename <- outfile
+
+  if (!file.exists(file.path(outfold, filename)) | overwrite) {
+
+    # Code reference:
+    # https://www.r-bloggers.com/2018/03/automatically-importing-publications-from-bibtex-to-a-hugo-academic-blog/
+    fileConn <- file.path(outfold, filename)
+    write("
+---
+title: 'Forecasting review bibliography'
+output:
+  word_document: default
+  html_document: default
+---
+
+```{r, echo = FALSE}
+knitr::opts_chunk$set(collapse = TRUE)
+```
+            ", fileConn)
+
+    for (i in 1:nrow(mypubs)) {
+      x <- mypubs[i, ]
+
+      # Authors. Comma separated list, e.g. `["Bob Smith", "David Jones"]`.
+      author <- str_replace_all(x["author"], " and ", ", ")
+      write(paste0(
+        "- ",
+        author,
+        ". ",
+        x[["year"]],
+        ". ",
+        x[["title"]],
+        ". ",
+        x[["journal"]],
+        ". ",
+        ifelse(!is.na(x[["volume"]]), paste0(x[["volume"]]), ""),
+        ifelse(!is.na(x[["number"]]), paste0("(", x[["number"]], "), "), ""),
+        ifelse(!is.na(x[["pages"]]), paste0(x[["pages"]], ", "), ""),
+        ifelse(!is.na(x[["doi"]]), paste0(x[["doi"]]), "")
+      ),
+
+      fileConn,
+      append = T
+      )
+
+      write(paste0(" "), fileConn, append = T)
+
+      # Abstract
+      if (abstract) {
+        write(paste0(x[["abstract"]]), fileConn, append = T)
+      }
+
+      write(paste0(" "), fileConn, append = T)
+    }
+  }
+}
--- Rscript/run_vignettes.R
+++ Rscript/run_vignettes.R
@@ -1,13 +1,35 @@
-
+devtools::load_all()
 library(rmarkdown)
 library(here)
+
+
+# Create bibliography .Rmd -----------------------------------------------------
+
+my_bibfile <- here::here("vignettes", "01_litreview", "bibliography.bib")
+my_outfold   <- here::here("vignettes", "01_litreview")
+my_outfile <- "01_003_Bibliography.Rmd"
+
+
+bibtex2rmd(bibfile = my_bibfile,
+           outfold = my_outfold,
+           outfile = my_outfile,
+           abstract = TRUE,
+           overwrite = TRUE)
+
+
+# Update package documentation ----------------------------------------------------
+
+devtools::document()
+
+# Render .Rmd files -------------------------------------------------------
 
 rmd_file <- list.files(here::here("Vignettes", "01_litreview"), pattern=".Rmd")
 
 for (i in seq_along(rmd_file)){
-  render(input = here::here("Vignettes", "01_litreview", rmd_file[i]), c("html_document", "word_document"))
+  rmarkdown::render(input = here::here("Vignettes", "01_litreview", rmd_file[i]), c("html_document", "word_document"))
 }
 
-
+# Update pkgdown YAML file and build the website ------------------------------------------------
+pkgdown::template_navbar()
 
 pkgdown::build_site()
