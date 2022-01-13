8b16af4283460ae57720ceadf7d48efa5f45f170
Bai Li <bai.li@noaa.gov>
2022-01-13 16:00:33 -0500

    add figures of BAM outputs

---
+++ Rscript/plot_bam_output.R
@@ -0,0 +1,93 @@
+# Plot BAM outputs using FishGraph (http://doi.org/10.7289/V5HT2M98)
+
+# install.packages("devtools")
+
+library(devtools)
+devtools::install_github("RobCheshire-NOAA/FishGraph")
+library(FishGraph)
+
+working_dir <- here::here("data", "AtlanticMenhadenSA")
+
+## Issues: could not load FishGraph properly, the only thing I can find in FishGraph is the gag dataset. So need to download the R folder from the FishGraph repository and source all the R files for now.
+
+download.file(url = "https://github.com/RobCheshire-NOAA/FishGraph/archive/refs/heads/master.zip",
+              destfile = here::here("data", "FishGraph-master.zip"))
+
+# unzip the .zip file
+unzip(zipfile = here::here("data", "FishGraph-master.zip"))
+
+# source the files
+Rfiles <- list.files(here::here("FishGraph-master", "R"))
+sapply(here::here("FishGraph-master", "R", Rfiles), source,.GlobalEnv)
+
+##### Start fresh ###########
+graphics.off()
+.SavedPlots <- NULL
+
+
+
+##### Read in the data from the ASCII .rdat file: #####
+##### Commented out in this example because FishGraph already has the menhaden data set in memory.
+menhaden <- dget(file.path(working_dir, "am019.rdat"))
+
+##### Common arguments in FishGraph functions. Convenient to define them once.
+ptype=NULL #NULL (no quotes) for no plots saved; other options: "pdf", "wmf", "eps"
+dtype=TRUE  #draft type
+ctype=TRUE  #color type
+########## Open a graphics device ###########
+windows(width = 8, height = 8, record = TRUE)
+pdf(file = file.path(working_dir, "bam_all_figures.pdf"), onefile = T)
+
+########## Call the functions #########################
+
+BSR.time.plots(menhaden, start.drop = 10, draft=dtype, use.color=ctype, graphics.type=ptype, legend.pos="top")
+
+#windows(width = 10, height = 8, record = TRUE)
+# Comp.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype, corr=T, c.min=0.2) #Error in Comp.plots(menhaden, draft = dtype, use.color = ctype, graphics.type = ptype,  :object 'm12dp' not found
+
+#windows(width = 8, height = 10, record = TRUE)
+Comp.yearly.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype, plot.neff=FALSE, print.neff=FALSE,
+                  compact = TRUE, print.n=TRUE)
+
+Cohort.plots(menhaden,draft=dtype, graphics.type=ptype)
+
+Index.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype)
+
+menhaden$parms$F40=0.54 #add to parms, calculated post-assessment
+menhaden$parms$F50=0.38 #add to parms, calculated post-assessment
+F.time.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype,
+             F.references=list(a="F40",b=c("Fmsy","F50")))
+
+Landings.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype,
+               L.units=c("1000 lb gutted","1000 lb gutted","1000 fish","1000 fish"), D.units="1000 dead fish", L.obs.pre=FALSE)
+
+Growth.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype, plot.all = TRUE)
+
+#windows(width = 8, height = 6, record = TRUE)
+# PerRec.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype,
+#              user.PR = list("SPR", "ypr.lb.gutted"), F.references=list("Fmsy","F40")) #Error in min(x) : invalid 'type' (list) of argument
+
+Eq.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype,
+         F.references=list("Fmsy", "F40"))
+
+StockRec.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype,
+               draw.lowess = FALSE, start.drop = 10, units.rec="number age-1 fish")
+
+#windows(width = 6, height = 4, record = TRUE, xpos = 10, ypos = 10)
+Selectivity.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype, plot.points=T)
+
+NFZ.age.plots(menhaden,draft=dtype, use.color=ctype, graphics.type=ptype,
+              user.plots="N.age.mdyr", start.drop=10)
+
+CLD.total.plots(menhaden, draft=dtype, use.color=ctype, graphics.type=ptype, first.year = "1965",
+                units.CLD.w="1000 lb gutted", CLD.w.references=list(NULL,"msy.klb", NULL),
+                plot.proportion = TRUE)
+
+#windows(width = 8, height = 8, record = TRUE)
+Phase.plots(menhaden, start.drop=10, draft=dtype, use.color=ctype, graphics.type=ptype, year.pos=2)
+
+Parm.plots(menhaden, graphics.type=ptype)
+
+Bound.vec.plots(menhaden, draft=dtype, graphics.type=ptype)
+dev.off()
+graphics.off()
