e39394f5839c78e7e3790dac0f64a653a431b1ab
Bai Li <bai.li@noaa.gov>
2022-11-08 12:55:51 -0500

    feat: add a function to read EwE MSY reference points

--- R/read_ewe_output.R
+++ R/read_ewe_output.R
@@ -4,7 +4,8 @@
 #'
 #' @param file_path a character string shows path to the working folder where EwE output csv files are located.
 #' @param file_names a vector of csv file names.
-#' @param skip_nrows interger: the number of lines of the data file to skip before reading data.
+#' @param skip_nrows integer: the number of lines of the data file to skip before reading data.
+#' @param colname_1 column name of the first column. Default name is Year. The other option is FM (fishing mortality) for reading output from FMSY analysis.
 #' @param functional_groups a character string describes the column of the data matrix, which is functional groups in EwE case.
 #' @param plot logical: if `TRUE` then make figures for all output data files.
 #' @param figure_titles a character vector of strings describes the names of each figure.
@@ -14,6 +15,7 @@
 read_ewe_output <- function(file_path,
                             file_names,
                             skip_nrows = 8,
+                            colname_1 = "Year",
                             functional_groups,
                             plot = FALSE,
                             figure_titles = NULL,
@@ -26,7 +28,7 @@ read_ewe_output <- function(file_path,
     data[[file_id]] <- read.table(
       text = as.character(data[[file_id]]),
       sep = ",",
-      col.names = c("Year", functional_groups)
+      col.names = c(colname_1, functional_groups)
     )
   }
 
@@ -39,7 +41,7 @@ read_ewe_output <- function(file_path,
       par(mar = c(5.1, 4.1, 4.1, 10), xpd = TRUE)
 
       plot(NULL,
-        xlab = "Year",
+        xlab = colname_1,
         ylab = figure_titles[figure_id],
         xlim = range(data[[figure_id]][, 1]),
         ylim = range(data[[figure_id]][, 2:ncol(data[[figure_id]])])
@@ -70,4 +72,55 @@ read_ewe_output <- function(file_path,
   }
 
   return(data)
+}
+
+#' Read in EwE MSY reference points and make figures
+#'
+#' @description It reads in the MSY reference points (i.e. MSY, FMSY, BMSY) from Ecopath with Ecosim (EwE) and making figures for yield over fishing mortality. It only works with Atlantic menhaden for now.
+#'
+#' @param file_path a character string shows path to the working folder where EwE output csv files are located.
+#' @param file_names a vector of csv file names.
+#' @param functional_groups a character string describes the column of the data matrix, which is functional groups in EwE case.
+#' @param key_functional_group a character string describes the key functional group.
+#' @param reference_points_scenario a character string describes the reference points scenario: compensation or staionary.
+#' @param ages age classes of the functional groups.
+#' @param plot logical: if `TRUE` then make figures for all output data files.
+#' @export
+read_ewe_reference_points <- function(file_path,
+                                      file_names,
+                                      functional_groups,
+                                      key_functional_group,
+                                      ages,
+                                      reference_points_scenario,
+                                      plot) {
+
+  msy_fleet <- read_ewe_output(
+    file_path = file_path,
+    file_names = file_names,
+    skip_nrows = 12,
+    colname_1 = "FM",
+    plot = FALSE,
+    figure_titles = NULL,
+    functional_groups = functional_groups,
+    figure_colors = NULL
+  )
+
+  catch_sum <- apply(msy_fleet[[1]][, paste0(key_functional_group, ages)], 1, sum)
+  MSY <- max(catch_sum) * 1000000
+  FMSY <- msy_fleet[[1]]$FM[which.max(catch_sum)]
+  biomass_sum <- apply(msy_fleet[[2]][, paste0(key_functional_group, ages)], 1, sum) * 1000000
+  BMSY <- biomass_sum[which.max(catch_sum)]
+  MSY_data <- list(MSY = MSY, FMSY = FMSY, BMSY = BMSY)
+
+  if (plot == TRUE) {
+    pdf(file = file.path(file_path, paste0(reference_points_scenario, "_yield_over_F.pdf")), onefile = T)
+    plot(msy_fleet[[1]]$FM, catch_sum,
+         xlab = "F", ylab = "Yield",
+         pch = 16, type="o")
+    abline(h = MSY, lty = 2)
+    abline(v = FMSY, lty = 3)
+    dev.off()
+  }
+
+  return(MSY_data)
 }
